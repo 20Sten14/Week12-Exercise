1bbeea2ebde4fdb0bd920cf15543cf22401a65e0
Opilane TTHK <opilane@tthk.ee>
2021-11-17 12:54:29 +0200

    fkasdh

---
+++ RandomQoute/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.IO;
+
+namespace RandomQoute
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("49 Wise Quotes That Will Inspire You to Success in Life");
+        Console.WriteLine(GetRandomJoke());
+        }
+            
+
+        public static string GetRandomJoke()
+        {
+            string fullfilepath = @"C:\Users\opilane\samples\quotes-file.txt
+
+";
+            string[] jokeAboutchuck = File.ReadAllLines(fullfilepath);
+            Random rnd = new Random();
+            int randomIndex = rnd.Next(0, jokeAboutchuck.Length);
+
+            return (jokeAboutchuck[randomIndex]);
+
+        }
+
+
+    }
+}
---
+++ ToDoList/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace ToDoList
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string directory = @"C:\Users\opilane\samples\titpe\ToDoList.txt";
+            string filepath = directory + @"\ToDoList.txt";
+
+            List<string> ToDo = new List<string>();
+            bool fileExists = file.exists
+
+
+
+            /*List<string> myWishes = new List<string>();
+            string fullfilepath = @"C:\Users\opilane\samples\titpe\Chores";
+            string[] dataFromFile = File.ReadAllLines(fullfilepath);
+            Console.WriteLine("Enter your to do list!");
+            string chore = Console.ReadLine();
+
+            string filepath = fullfilepath + @"\MyFiles.txt";
+            foreach (string line in dataFromFile)
+            {
+                File.Create($@"{fullfilepath}\{line}.txt");
+            }*/
+
+
+
+            /*myWishes.Add("fresh new cool mints");
+            myWishes.Add("mercury");
+            myWishes.Add("gaming chair");
+
+            int listLenght = myWishes.Count;
+            Console.WriteLine($"List lenght : {listLenght}");
+            //Console.WriteLine(myWishes[0]);
+            //Console.WriteLine(myWishes[1]);
+            //Console.WriteLine(myWishes[2]);
+
+            foreach (string wish in myWishes)
+            {
+                Console.WriteLine($"you wish for : {wish}");*/
+
+        
+        }
+    }
+}
